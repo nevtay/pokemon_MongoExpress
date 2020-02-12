0df42c45083572bd9ce4e6e1336fa15df9403b82
nevtay <nevintay@gmail.com>
2020-02-12 14:19:22 +0800

    bug fix

--- src/utils/db.js
+++ src/utils/db.js
@@ -8,7 +8,7 @@ const mongoOptions = {
 }
 
 const dbName = "labPokedex"
-const dbUrl = process.env.MONGO_URI || "mongodb://localhost:27017/" + dbName; 
+const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/" + dbName; 
 mongoose.connect(dbUrl, mongoOptions);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"))
